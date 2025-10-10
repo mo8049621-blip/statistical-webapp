81f09a95798348619ca27a0bf796cb4339a66101
Piglet <125090703@link.cuhk.edu.cn>
2025-10-10 15:49:41 +0800

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   // 本地开发时使用根路径
   // 部署到GitHub Pages时，需要修改为你的仓库名称
   // 例如：如果仓库地址是 https://github.com/username/repo-name，则设置为 '/repo-name/'
-  base: '/',
+  base: '/statistical-webapp/',
 })
